2b7060d5fa9c724bcbbe760bf75f3958bccf229b
Creedmastr <thegaminglife09@gmail.com>
2023-04-04 18:34:15 +0200

    added tests

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,27 @@ fn main() {
 
     println!("{:#?}", entries::get_entries::get_entries());
 }
+
+
+#[cfg(test)]
+mod test {
+    use crate::entries::{entry::Entry, get_entries};
+
+    use super::*;
+
+    #[test]
+    fn test_add_entry_and_get_entry() {
+        init::init();
+
+        add_entry("test".to_string());
+        let entries = get_entries::get_entries();
+
+        assert_eq!(
+            Entry {
+                id: 0,
+                content: "test".to_string()
+            }.content,
+            entries[0].content.replace(" ", "") // I know it works correctly, because there's always a space at the end (for the moment)
+        );
+    }
+}
